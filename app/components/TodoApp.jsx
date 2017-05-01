07755f7aa560d01cc3c58e747e6cce9189f992b5
Daniel Straus <strausd@me.com>
2017-05-01 18:38:29 -0500

    Refactor app to work with local storage

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -5,49 +5,16 @@ var moment = require('moment');
 import TodoList from 'TodoList';
 import AddTodo from 'AddTodo';
 import TodoSearch from 'TodoSearch';
-var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-    getInitialState: function () {
-        return {
-            showCompleted: true,
-            searchText: '',
-            todos: TodoAPI.getTodos()
-        };
-    },
-    componentDidUpdate: function () {
-        TodoAPI.setTodos(this.state.todos);
-    },
-    handleAddTodo: function (text) {
-        this.setState({
-            todos: [
-                ...this.state.todos,
-                {
-                    id: uuid(),
-                    text: text,
-                    completed: false,
-                    createdAt: moment().unix(),
-                    completedAt: undefined
-                }
-            ]
-        });
-    },
-    handleSearch: function (showCompleted, searchText) {
-        this.setState({
-            showCompleted: showCompleted,
-            searchText: searchText
-        });
-    },
     render: function () {
-        var {todos, showCompleted, searchText} = this.state;
-        var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
         return (
             <div className="ui stackable grid centered">
                 <div className="eight wide computer twelve wide tablet tablet sixteen wide mobile column">
                     <h1 className="main-header">Todo App</h1>
-                    <TodoSearch onSearch={this.handleSearch}/>
+                    <TodoSearch/>
                     <TodoList/>
-                    <AddTodo onAddTodo={this.handleAddTodo}/>
+                    <AddTodo/>
                 </div>
             </div>
         );
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -56,5 +56,23 @@ describe('Reducers', () => {
             expect(res[0].completed).toEqual(true);
             expect(res[0].completedAt).toNotBe(undefined);
         });
+
+        it('should add existing todos', () => {
+            var todos = [{
+                id: '111',
+                text: 'Anything',
+                completed: false,
+                completedAt: undefined,
+                createdAt: 33000
+            }];
+            var action = {
+                type: 'ADD_TODOS',
+                todos
+            };
+            var res = reducers.todosReducer(df([]), df(action));
+
+            expect(res.length).toEqual(1);
+            expect(res[0]).toEqual(todos[0]);
+        });
     });
 });
